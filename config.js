3eda8bc6915034c6f15792dea4d6c74f70b294c5
fuad <98115198+Fuadboters@users.noreply.github.com>
2022-02-04 10:24:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,10 +15,10 @@ gc1 = ''
 gc2 = ''
 gc3 = ''
 global.linkGC = ['https://chat.whatsapp.com/HJ9pD2XJ8vq7whuAYOvT1Y', ''] // ganti jadi group lu
-global.owner = ['+1 (775) 403-5736','6289505165400'] // Put your number here //owner eval
-global.kontak = ['+1 (775) 403-5736','6289505165400'] //Ketika ada yang ngetik #owner
+global.owner = ['+62 858-6718-5837','6283890193709'] // Put your number here //owner eval
+global.kontak = ['+62 858-6718-5837','+6283890193709'] //Ketika ada yang ngetik #owner
 global.mods = ['0'] // Want some help?
-global.prems = ['+1 (775) 403-5736','6289505165400','6282287750102'] // Premium user has unlimited limit
+global.prems = ['+62 858-6718-5837','6283890193709'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com', 
