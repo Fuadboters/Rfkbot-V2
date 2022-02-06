a6a3e1fd101a3d32809d44f24579f80137b10771
fuad <98115198+Fuadboters@users.noreply.github.com>
2022-02-06 22:35:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,10 +15,10 @@ gc1 = ''
 gc2 = ''
 gc3 = ''
 global.linkGC = ['https://chat.whatsapp.com/HJ9pD2XJ8vq7whuAYOvT1Y', ''] // ganti jadi group lu
-global.owner = ['+62 858-6718-5837'] // Put your number here //owner eval
-global.kontak = ['+62 858-6718-5837'] //Ketika ada yang ngetik #owner
+global.owner = ['+62 858-6718-5837','6285867185837'] // Put your number here //owner eval
+global.kontak = ['+62 858-6718-5837','6285867185837'] //Ketika ada yang ngetik #owner
 global.mods = ['0'] // Want some help?
-global.prems = ['+62 858-6718-5837'] // Premium user has unlimited limit
+global.prems = ['+62 858-6718-5837','6285867185837'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com', 
@@ -50,7 +50,7 @@ namaig = 'GAK ADA :)'
 namagithub = 'GAK ADA :)'
 kasihcaption = `Nih kak`
 namakontak1 = 'FuadOFC'
-
+namakontak2 ='FuadOFC'
 //kasihcaption = `Nih Kak`
 
 // Sticker WM
